8c56f7857c915c0254cc008c806b27d77dbd6366
JHiika <justiine.berthet@gmail.com>
2022-09-19 15:39:02 +0200

    feat: bootstrap, css and navbar on mainpage

---
+++ src/components/HomeNavbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <img src="../assets/Manga.png" style="width:100px" alt="..." />
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0 text-color-header">
+                        <li class="nav-item"><router-link to="/">Accueil</router-link></li>
+                        <li class="nav-item"><router-link to="/list">Liste de manga</router-link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</template>
---
+++ src/views/MangaList.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="manga">
+      <h1>This is an manga page</h1>
+    </div>
+</template>
+  
